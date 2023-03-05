aa0d0edac03552b2bda5a36cf51585d2a6a2b973
Pratyaksh <72778974+pratyaksh29@users.noreply.github.com>
2023-03-05 17:23:00 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:telephony/telephony.dart';
 import 'dart:async';
-
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 Future<dynamic> _handleIncomingMessageInBackground(SmsMessage message) async {
   return Future.value(true);
@@ -16,22 +17,131 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   String _lastMessage = '';
-  String _receiveAt = '+919811312590';
+  String _receiveAt = '(NUMBER)';
 
-  Future<String> processMessage(String message) async {
-    String response = '';
-    if (message.toLowerCase() == 'hi') {
-      response = 'Hello';
-    } else {
-      response = "I am currently not configured to provide any response. Please try again later.";
-    }
-    return response;
-  }
+  final _promptController = TextEditingController();
 
-  Future<void> sendMessage(String message) async {
+  Future<void> sendMessage(List<String> messages, {String? to}) async {
     final telephony = Telephony.instance;
-    await telephony.sendSms(to: _receiveAt, message: message);
+    print(messages.length);
+    for (var i = 0; i < messages.length; i++) {
+      final message = messages[i];
+      print('Sending message ${i + 1}/${messages.length}: $message');
+      await telephony.sendSms(to: to ?? _receiveAt, message: message);
+      print('Message ${i + 1} sent successfully!');
+    }
   }
+
+
+
+
+
+  Future<List<String>> processMessage(String message) async {
+    List<String> responses = [];
+    var url;
+    var headers = {'Content-Type': 'application/json'};
+    var body;
+
+    String sms = message;
+    final splitted = sms.split(' ');
+    bool isEmergency = false;
+    bool isMother = false;
+
+    for (final word in splitted) {
+      if (word == "emergency") {
+        isEmergency = true;
+        break;
+      }
+    }
+
+
+    for (final word in splitted) {
+      if (word == "mother") {
+        isMother = true;
+        break;
+      }
+    }
+
+    if (isEmergency) {
+      url = Uri.parse('https://ba37-136-233-9-98.in.ngrok.io/query/emergency');
+      body = json.encode({
+        "phNumber": "(NUMBER)",
+      });
+      await http.post(url, body: body);
+
+      url = Uri.parse('https://ba37-136-233-9-98.in.ngrok.io/query');
+      body = json.encode({
+        "phNumber": "(NUMBER)",
+        "prompt": message,
+        "location": "VIT Vellore",
+      });
+    }
+    else if(isMother){
+      url = Uri.parse('https://ba37-136-233-9-98.in.ngrok.io/mother/addmother');
+      body = json.encode({
+        "phNumber": "(NUMBER)",
+        "natalSupport": false ,
+      });
+      await http.post(url, body: body);
+
+      // url = Uri.parse('https://ba37-136-233-9-98.in.ngrok.io/query');
+      // body = json.encode({
+      //   "phNumber": "(NUMBER)",
+      //   "prompt": message,
+      //   "location": "VIT Vellore",
+      // });
+    }
+
+    else {
+      url = Uri.parse('https://ba37-136-233-9-98.in.ngrok.io/query');
+      body = json.encode({
+        "phNumber": "(NUMBER)",
+        "prompt": message,
+        "location": "VIT Vellore",
+      });
+    }
+
+
+    var apiResponse = await http.post(url, headers: headers, body: body);
+    if (apiResponse.statusCode == 200) {
+      Map<String, dynamic> jsonResponse = json.decode(apiResponse.body);
+      String translatedText = jsonResponse["translatedText"];
+      responses = translatedText.split('\n').where((line) => line.trim().isNotEmpty).toList();
+    } else {
+      responses = ['Error: ${apiResponse.statusCode}'];
+    }
+
+    print(responses);
+    return responses;
+  }
+
+
+
+
+
+
+  // Future<List<String>> processMessage(String message) async {
+  //   List<String> responses = [];
+  //   var url = Uri.parse('https://ba37-136-233-9-98.in.ngrok.io/query');
+  //   var headers = {'Content-Type': 'application/json'};
+  //   var body = json.encode({
+  //     "phNumber": "(NUMBER)",
+  //     "prompt": message,
+  //     "location": "asd",
+  //   });
+  //   var apiResponse = await http.post(url, headers: headers, body: body);
+  //   if (apiResponse.statusCode == 200) {
+  //     Map<String, dynamic> jsonResponse = json.decode(apiResponse.body);
+  //     String translatedText = jsonResponse["translatedText"];
+  //     responses = translatedText.split('\n').where((line) => line.trim().isNotEmpty).toList(); // split response into separate messages
+  //   } else {
+  //     responses = ['Error: ${apiResponse.statusCode}'];
+  //   }
+  //
+  //   print(responses);
+  //   return responses;
+  // }
+
 
   @override
   Widget build(BuildContext context) {
@@ -43,11 +153,19 @@ class _MyAppState extends State<MyApp> {
             children: <Widget>[
               Text('Last Message Received:'),
               Text(_lastMessage),
+              TextField(
+                controller: _promptController,
+                decoration: InputDecoration(
+                  hintText: 'Enter prompt',
+                ),
+              ),
+
               ElevatedButton(
                 child: Text('Send Message'),
                 onPressed: () async {
-                  String response = await processMessage('hi');
-                  await sendMessage(response);
+                  String prompt = _promptController.text;
+                  List<String> responses = await processMessage(prompt);
+                  await sendMessage(responses);
                 },
               ),
             ],
@@ -66,17 +184,21 @@ class _MyAppState extends State<MyApp> {
     );
   }
 
-
-
   void _handleIncomingMessage(SmsMessage message) async {
     if (message.address == _receiveAt) {
-      String response = await processMessage(message.body!);
-      await sendMessage(response);
+      List<String> responses = await processMessage(message.body!);
+      for (String response in responses) {
+        await sendMessage(responses);
+      }
+
+      print(message);
       setState(() {
-        _lastMessage = message.body ?? '';
+        _lastMessage = message.body!;
       });
     }
   }
+
+
 
   @override
   void initState() {
